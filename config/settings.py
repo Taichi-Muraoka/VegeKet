c9dac23126a1f2ccef42dab05ed8e7e1e16c9c70
Taichi <dedenne.taichi@gmail.com>
2025-04-14 16:34:23 +0900

    コンテキストプロセッサー

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'config.custom_context_processors.base',  # 追記
             ],
         },
     },
@@ -164,3 +166,6 @@ MESSAGE_TAGS = {
     messages.INFO: 'rounded-0 alert alert-info',
     messages.DEBUG: 'rounded-0 alert alert-secondary',
 }
+
+# custom_context_processors
+TITLE = 'VegeKet'
